e8e53a3d0c57fbb782789187d8c9d14ba23878d2
WispaRus <89263219040@yandex.ru>
2021-11-10 17:17:43 +0300

    Делаем домашку к 2 уроку

---
+++ src/main/java/Level1/lesson02/Main.java
@@ -0,0 +1,21 @@
+package Level1.lesson02;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        System.out.print();
+    }
+        //region Задача 1
+        public static void invertArray() {
+            int[] arr = { 1, 0, 1, 0, 0, 1 };
+            for (int i = 0; i < arr.length; i++)
+                if (i == 0) {
+                    i = 1;
+                } else i = 0;
+        //endregion
+        }}
+
+
+
+
