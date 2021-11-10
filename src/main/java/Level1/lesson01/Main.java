5fcd8349ff94f28648cb208e4c125023e8d607be
WispaRus <89263219040@yandex.ru>
2021-11-10 11:29:52 +0300

    Сдалали домашку к 1 уроку

--- src/main/java/Level1/lesson01/Main.java
+++ src/main/java/Level1/lesson01/Main.java
@@ -14,56 +14,55 @@ public class Main {
                 boolean booleanVar = false;
     //endregion
         System.out.println(calculate(2, 2, 2, 2));
-        System.out.println(task10and20(5,6));
+        System.out.println(task10and20(10, 11));
         isPositiveOrNegative(-30);
-        isNegative(1);
+        isNegative(-1);
+        System.out.println("Почему задача 6 не выводится в консоль?");
+        printMyText("Никита");
+
     }  //endregion
-    //region Задача 3
+        //region Задача 3
+        public static float calculate(int a, int b, int c, int d) {
+          float result = a * (b + (c / d));
+          return result;
 
-    public static float calculate(int a, int b, int c, int d) {
-      float result = a * (b + (c / d));
-      return result;
-
-    }
-
-
-    //endregion
-    //region Задача 4
-    /*
-    Написать метод, принимающий на вход два целых числа и проверяющий, что их сумма лежит в пределах от 10 до 20 (включительно),
-    если да – вернуть true, в противном случае – false.
-     */
-    public static boolean task10and20(int x1, int x2) {
-        if ((x1 + x2) >= 10 && (x1 + x2) <= 20) {
-            return true;
-        } else return false;
-    }
-
-
-
-
-    //endregion
-    //region Задача 5
-
-    public static void isPositiveOrNegative(int x) {
-        if(x > 0) {
-            System.out.println("Positive");
-        } else {
-            System.out.println("Negative");
         }
-    }
+        //endregion
+            //region Задача 4
+            /*
+            Написать метод, принимающий на вход два целых числа и проверяющий, что их сумма лежит в пределах от 10 до 20 (включительно),
+            если да – вернуть true, в противном случае – false.
+             */
+            public static boolean task10and20(int x1, int x2) {
+                if ((x1 + x2) >= 10 && (x1 + x2) <= 20) {
+                    return true;
+                } else return false;
+            }
+            //endregion
+                //region Задача 5
+                public static void isPositiveOrNegative(int x) {
+                    if(x > 0) {
+                        System.out.println("Positive");
+                    } else {
+                        System.out.println("Negative");
+                    }
+                }
+                //endregion
+                    //region Задача 6
 
+                    public static boolean isNegative(int value) {
+                        if (value < 0) {
+                            return true;
+                        } else return false;
+
+                    }
 
     //endregion
-    //region Задача 6
-
-    public static boolean isNegative(int x3) {
-        if(x3 > 0) {
-            return false;
-        }
-            return true;
-    }
-
-
-    //endregion
+                        //region Задача 7
+                        public static void printMyText(String txtToPrint) {
+                            System.out.println("Привет, " +txtToPrint +"!");
+                        }
+                        //endregion
 }
+
+
